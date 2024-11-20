2a15726c9e815c702f9997f6b2c36e7b2c89b952
Kory291 <01lukasschaefer@gmail.com>
2024-11-21 00:25:18 +0100

    first try for footer

--- stern-marzahn-website/src/app/app.routes.ts
+++ stern-marzahn-website/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+import { ImprintComponent } from './imprint/imprint.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "datenschutzerklaerung", component: PrivacyPolicyComponent},
+    {path: "impressum", component: ImprintComponent}
+];
