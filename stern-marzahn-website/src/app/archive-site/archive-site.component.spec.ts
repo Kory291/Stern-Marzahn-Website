763a6a2b4e3620795598f69aed9a2cbd85fad41a
Kory291 <01lukasschaefer@gmail.com>
2024-11-29 20:13:41 +0100

    add first setup for rooting

---
+++ stern-marzahn-website/src/app/archive-site/archive-site.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ArchiveSiteComponent } from './archive-site.component';
+
+describe('ArchiveSiteComponent', () => {
+  let component: ArchiveSiteComponent;
+  let fixture: ComponentFixture<ArchiveSiteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ArchiveSiteComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ArchiveSiteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ stern-marzahn-website/src/app/archive-site/archive-site.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-archive-site',
+  standalone: true,
+  imports: [],
+  templateUrl: './archive-site.component.html',
+  styleUrl: './archive-site.component.sass'
+})
+export class ArchiveSiteComponent {
+
+}
