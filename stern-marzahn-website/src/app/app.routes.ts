66cefd4225952236b33fc2f2a6a0da0d6e5000f3
Kory291 <01lukasschaefer@gmail.com>
2024-12-01 19:52:30 +0100

    add header

--- stern-marzahn-website/src/app/app.routes.ts
+++ stern-marzahn-website/src/app/app.routes.ts
@@ -4,11 +4,16 @@ import { ImprintComponent } from './imprint/imprint.component';
 import { ContactComponent } from './contact/contact.component';
 import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ArchiveSiteComponent } from './archive-site/archive-site.component';
+import { FurtherLinksAndRulesComponent } from './further-links-and-rules/further-links-and-rules.component';
 
 export const routes: Routes = [
     {path: "", component: LandingPageComponent},
     {path: "datenschutzerklaerung", component: PrivacyPolicyComponent},
     {path: "impressum", component: ImprintComponent},
     {path: "kontakt", component: ContactComponent},
-    {path: "nutzungsbedingungen", component: TermsOfUseComponent}
+    {path: "nutzungsbedingungen", component: TermsOfUseComponent},
+    {path: "archiv", component: ArchiveSiteComponent},
+    {path: "startseite", component: LandingPageComponent},
+    {path: "weitere-links-und-regeln", component: FurtherLinksAndRulesComponent}
 ];
