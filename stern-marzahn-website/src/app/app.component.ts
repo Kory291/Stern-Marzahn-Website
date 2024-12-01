a221d398689cd4399c8702383b95ca8168b19e37
Kory291 <01lukasschaefer@gmail.com>
2024-12-01 19:30:02 +0100

    add sidebar

--- stern-marzahn-website/src/app/app.component.ts
+++ stern-marzahn-website/src/app/app.component.ts
@@ -2,14 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
 export class AppComponent {
-  title = 'stern-marzahn-website';
+  title = 'Stern Marzahn';
 }
--- stern-marzahn-website/src/app/app.routes.ts
+++ stern-marzahn-website/src/app/app.routes.ts
@@ -1,8 +1,14 @@
 import { Routes } from '@angular/router';
 import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 import { ImprintComponent } from './imprint/imprint.component';
+import { ContactComponent } from './contact/contact.component';
+import { TermsOfUseComponent } from './terms-of-use/terms-of-use.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
+    {path: "", component: LandingPageComponent},
     {path: "datenschutzerklaerung", component: PrivacyPolicyComponent},
-    {path: "impressum", component: ImprintComponent}
+    {path: "impressum", component: ImprintComponent},
+    {path: "kontakt", component: ContactComponent},
+    {path: "nutzungsbedingungen", component: TermsOfUseComponent}
 ];
